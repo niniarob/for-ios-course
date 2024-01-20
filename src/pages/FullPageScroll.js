f9d4db2698e29ee2cb3d5bf2a362aa3b766fe8c0
niniarob <robakidzenino89@gmail.com>
2024-01-20 22:24:48 +0400

    fix error

--- src/pages/FullPageScroll.js
+++ src/pages/FullPageScroll.js
@@ -206,12 +206,13 @@ const FullPageScroll = () => {
                 viewport={{ once: false, amount: 0.3 }}
                 className="img-style gestures-img"
               ></motion.div>
-              <motion.p 
-                              variants={fadeIn("up", 0.3)}
-                              initial="hidden"
-                              whileInView={"show"}
-                              viewport={{ once: false, amount: 0.3 }}
-              className="gestures">
+              <motion.p
+                variants={fadeIn("up", 0.3)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.3 }}
+                className="gestures"
+              >
                 Gestures :
                 <span>
                   მიუხედავად იმისა, რომ Apple Pencil-ი საუკეთესოდ მუშაობს
@@ -233,10 +234,110 @@ const FullPageScroll = () => {
           </div>
         </FullpageSection>
         <FullpageSection className="fullpage-section bck-img-five same-style-bck-img">
-          <h1>shevzleb</h1>
+          <div className="content">
+            <div className="gestures-content ">
+              <motion.p
+                variants={fadeIn("down", 0.3)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.3 }}
+                className="gestures"
+              >
+                User Friendly :
+                <span>
+                  {" "}
+                  გასათვალისწინებელია ის ფაქტი, რომ Procreate მუდმივად
+                  ითვალისწინებს მომხმარებელთა რეკომენდაციებს და ხვეწავს აპს,
+                  იმისდა მიხედვით თუ რის საშუალებას აძლევს iOS(HIG)-ი. Procreate
+                  გვთავაზობს ნათელ და ლაკონურ მითითებებს აპპ-ში სამოძრაოდ, არ
+                  დაკარგა თანმიმდევრულობა, რაც უზრუნველყოფს ნაცნობ გარემოს,
+                  რომელიც მომხმარებლებს ეხმარება ნავიგაციაში და სხვადასხვა
+                  აპებთან ურთიერთქმედებაში, რაც მეტად კომფორტულია. ფენების
+                  მენეჯმენტი, აპლიკაცია მოიცავს ფენების მართვის მძლავრ
+                  ფუნქციებს, რაც მომხმარებლებს გაუადვილებს მრავალ ფენასთან
+                  მუშაობას, მათ გადაწყობას, გამჭვირვალობის რეგულირებას და
+                  შერწყმის რეჟიმების გამოყენებას. ეს მოქნილობა გადამწყვეტია
+                  ციფრული მხატვრებისთვის, რომლებიც მუშაობენ რთულ ილუსტრაციებზე.
+                </span>
+              </motion.p>
+              <motion.div
+                variants={fadeIn("up", 0.3)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.3 }}
+                className="img-style gestures-img user-friend-img"
+              ></motion.div>
+            </div>
+          </div>
         </FullpageSection>
         <FullpageSection className="fullpage-section bck-img-six same-style-bck-img">
-          <h1>shevzleb</h1>
+          <div className="content">
+            <motion.h1
+              variants={fadeIn("down", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.3 }}
+            >
+              . რეკომენდაციები:{" "}
+            </motion.h1>
+            <div className="lists">
+              <motion.div
+                variants={fadeIn("right", 0.3)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.3 }}
+                className="first-list-box gap"
+              >
+                <p>
+                  * Procrete მომხმარებლებს არ სთავაზობს სარეზერვო ასლს, ფაილები
+                  ინახება ადგილპობრივ გალერეაში აპის მთავარ გვერდზე იქამდე სანამ
+                  აპი ფუნქციონირებს,თუ აპი წაიშლება ნამუშევრებიც გაქრება,
+                  სიცხადისთვის კარგი იქნება თუ ეს პრობლემა აღმოიფხვრება.
+                </p>
+                <p>
+                  * Widgets ერთ-ერთი ყველაზე მაგარი ფუნქციაა რაც იპადს აქვს,
+                  თუმცა თუ გვინდა ეს Procreate-ში გამოვიყენოთ ან უბრალოდ გვინდა
+                  დაგვიანებები თავიდან ავირიდოთ მუშაობის პროცესში, ამისთვის
+                  აუცილებელია ipadOS 14 ან უფრო მაღალი ვერსია, ეს ნამდვილად არ
+                  არის ყველა მომხმარებლისთის კომფორტული და მისაღები.
+                </p>
+                <p>
+                  * რაც შეეხება ფენებს, სადაც ბევრ დადებით თვისებებთან ერთად
+                  აქვს გარკვეული შეძღუდვები დიდ გაღიზიანებას იწვევს
+                  მომხმარებლებში, ფენების რაოდენობა, რომელთა გამოყენებაც
+                  შეგვიძლია ჩვენს პროექტებში პირდაპირ დამოკიდებულია ტილოს
+                  დიზაინსა და ზომაზე, მაღტალია ipad-ის ახალი მოდელები
+                  გვთავაზობენ მეტ ფენას სამუშაოდ თუმცა აქაც არის გარკვეული
+                  შეზღუდვები.
+                </p>
+              </motion.div>
+              <motion.div
+                variants={fadeIn("left", 0.3)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.3 }}
+                className="second-list-box gap"
+              >
+                <p>
+                  * ვფიქრობ, საინტერესო და სასურველი იქნება დიზაინში თუ
+                  დაემატება ერთი გამოყოფილი ველი, სადაც დამწყები ილუსტრატორები,
+                  მხატვრები, ანიმატორები შეძლებენ თავიანთი ნამუშევრების
+                  გაზიარებას სხვა მეტად გამოცდილი მომხმარებლებისთვის,
+                  კომუნიკაცია და ინფორმაციის გაზიარება საერთო ინტერესების მქონე
+                  ხალხთან მეტად ლოიალურს და საინტერესოს გახდის აპს.
+                </p>
+                <p>
+                  * Procreate Potoshop-ის გაუმჯობესებული ვარიანტია, რომელმაც
+                  ipad-ის დიზაინი იდეალურად გამოიყენა და ყველაფერი იდეალურად
+                  განალაგა ეკრანზე, ბევრად კომფორტული გახადა მუშაობა თუმცა ცუდია
+                  ის ფაქტი რომ ეს აპლიკაცია მხოლოდ ხელმისაწვდომია ipad-ზე და
+                  iphone-ზე, ვფიქრობ macOS-ის ვერცია ძალიან მაგარი იქნება,
+                  ბევრად დიდი სამუშაო სივრცე, აღქმა და მარტივი გადაადგილება
+                  აპებს შორის.
+                </p>
+              </motion.div>
+            </div>
+          </div>
         </FullpageSection>
       </FullPageSections>
     </Fullpage>
