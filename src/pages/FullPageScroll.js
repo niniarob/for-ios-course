7db67d588bb239da5e9d00dd139e71141bf85689
niniarob <robakidzenino89@gmail.com>
2024-01-20 21:03:24 +0400

    add gestures section

--- src/pages/FullPageScroll.js
+++ src/pages/FullPageScroll.js
@@ -139,10 +139,98 @@ const FullPageScroll = () => {
           </div>
         </FullpageSection>
         <FullpageSection className="fullpage-section bck-img-three same-style-bck-img">
-          <h1>shevzleb</h1>
+          <div className="content">
+            <motion.h1
+              variants={fadeIn("down", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.3 }}
+            >
+              . განვიხილოთ Proreate app iOS (HIG)-ის მიხედვით.
+            </motion.h1>
+            <div className="des-img proreate-img-text">
+              <motion.div
+                variants={fadeIn("right", 0.3)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.3 }}
+                className="paragraph"
+              >
+                <p className="description proreate-desc">
+                  Layout :
+                  <span>
+                    {" "}
+                    Procreate-ს აპში სამი ნაწილია, რომელიც შექმნილია
+                    ნამუშევრებზე ფოკუსირების შესანარჩუნებლად. ზედა მარჯვენა
+                    ნაწილი, სადაც განთავსებულია ყველაფერი რაც ხატვის დასაწყება
+                    არის საჭირო, წაშლა, ფენები, ფერი, ხატვა და ასე შემდეგ.
+                    მოდიფიკაციის ხელსაწყოები ყველა ხელმისაწვდომია მარცხენა
+                    მხარეს, ფუნჯის ზომა/გამჭვირვალობა, ისრების
+                    გაუქმება/გამეორება, აგრეთვე მომხმარებელს შეუძლია გამოიყენოს
+                    Undo, Redo და Modify ღილაკები.
+                  </span>
+                </p>
+                <p className="description proreate-desc">
+                  Accessibility :
+                  <span>
+                    როგორც ჩვენთვის ცნობილია, Procreate ერთ-ერთი ყველაზე
+                    მეგობრული პლათფორმაა ეიფლის ეკოსისტემაში, Procreate-მ
+                    დაამატა ისეთი ფუნქციები,რომლებიც მხეველობა დარღვეულ ან
+                    დაქვეითებულ მომხმარებლებს უადვილებს აპში გადაადგილებას,
+                    ერთ-ერთია VoiceOver ამ ფუნქციის საშუალებით მომხმარებელს
+                    შეუძლია გაიგოს თუ რას დააჭიროს ხელი ნავიგაციისთვის,
+                    მენიუების წასაკითხად თუ სხვა მნიშვნელოვანი გვერდის
+                    გასახსნელად. Dynamic Type- რომელიც ფონტის ზომას არეგულირებს
+                    იმისდა მიხედვით თუ როგორ გვჭირდება და მეტად კომფორტულს ქმნის
+                    აპლიკაციას გამოსაყენებლად.{" "}
+                  </span>
+                </p>
+              </motion.div>
+              <motion.div
+                variants={fadeIn("left", 0.3)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.3 }}
+                className="img proreate-logo-img"
+              ></motion.div>
+            </div>
+          </div>
         </FullpageSection>
         <FullpageSection className="fullpage-section bck-img-four same-style-bck-img">
-          <h1>shevzleb</h1>
+          <div className="content">
+            <div className="gestures-content">
+              <motion.div
+                variants={fadeIn("down", 0.3)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.3 }}
+                className="img-style gestures-img"
+              ></motion.div>
+              <motion.p 
+                              variants={fadeIn("up", 0.3)}
+                              initial="hidden"
+                              whileInView={"show"}
+                              viewport={{ once: false, amount: 0.3 }}
+              className="gestures">
+                Gestures :
+                <span>
+                  მიუხედავად იმისა, რომ Apple Pencil-ი საუკეთესოდ მუშაობს
+                  Procreate-თან, ხატვის დაწყება თითითაც(თითები) შესაძლებელია,
+                  ყველაფერს რასაც Apple Pencil-ი გვთავზობს საჭიროების
+                  შემთხვევაში შგვიძლია გავაკეთოთ ჟესტის ენით, როგორიცაა
+                  გადიდება, დაპატარავება, გადაფურცვლა,შეცვლა და სხვა.
+                  დასაფასებელია ის ფაქტიც რომ Procreate-მ მხარი დაუჭირა
+                  მოძრაობის ფილტრაციას ტრემონის მქონე მომხმარებელთათვის.
+                  Procreate ხელს უწყობს ერთი შეხედვით ფუნქციონირებასაც, თუმცა
+                  ამასაც აქვს თავისი უარყოფითი მხარე, რადგან შეიძლება
+                  შემთხვევითი შეხებით გადაიფურცლოს ან გვერდი შეიცვალოს,
+                  თავდაპირველად Procreate-ს არ ჰქონდა Multitasking-ი და შემდგომ
+                  დაემატა, ეხლა უკვე შესაძლებელია რამდენიმე ფაილის გახსნა და
+                  ბევრად კომპორტულ გარემოში მუშაობა{" "}
+                </span>
+              </motion.p>
+            </div>
+          </div>
         </FullpageSection>
         <FullpageSection className="fullpage-section bck-img-five same-style-bck-img">
           <h1>shevzleb</h1>
